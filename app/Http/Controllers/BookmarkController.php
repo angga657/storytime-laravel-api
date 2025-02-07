86ce8db7d9cfb2b488b5eaad5fab39f8f298eee5
angga657 <117795076+angga657@users.noreply.github.com>
2025-02-07 15:52:46 +0800

    update: change field image

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -36,9 +36,9 @@ class BookmarkController extends Controller
 
         $formattedBookmarks = $bookmarks->map(function ($bookmark) {
             $book = $bookmark->book;
-            $imagePaths = is_string($book->images)
-                ? json_decode($book->images, true)
-                : ($book->images ?? []);
+            $imagePaths = is_string($book->image)
+                ? json_decode($book->image, true)
+                : ($book->image ?? []);
         
             $selectedImage = collect($imagePaths)->first();
         
@@ -94,9 +94,9 @@ class BookmarkController extends Controller
     
         $formattedBookmark = [
             'id' => $bookmark->id,
-            'images' => is_string($bookmark->book->images ?? null)
-                ? collect(json_decode($bookmark->book->images, true))->first()
-                : ($bookmark->book->images ?? null),
+            'images' => is_string($bookmark->book->image ?? null)
+                ? collect(json_decode($bookmark->book->image, true))->first()
+                : ($bookmark->book->image ?? null),
             'title' => $bookmark->book ? $bookmark->book->title : null,
             'username' => $bookmark->user ? $bookmark->user->username : null,
             'category' => $bookmark->book && $bookmark->book->category 
@@ -121,9 +121,9 @@ class BookmarkController extends Controller
         try {
             $book = Book::findOrFail($id);
 
-            $images = is_string($book->images) 
-                ? json_decode($book->images, true) 
-                : ($book->images ?? []);
+            $images = is_string($book->image) 
+                ? json_decode($book->image, true) 
+                : ($book->image ?? []);
 
             $selectedImage = collect($images)->first();
 
